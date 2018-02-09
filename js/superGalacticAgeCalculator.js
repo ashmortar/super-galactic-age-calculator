fba3ba70cf1a9c7fc788569b89ae2bbd2ede6da4
Aaron Ross <ashmortar@gmail.com>
2018-02-09 12:06:25 -0800

    refactor to make various things properties of the calculator object that are retrieved by getter functions

--- js/superGalacticAgeCalculator.js
+++ js/superGalacticAgeCalculator.js
@@ -1,20 +1,27 @@
 let moment = require('moment');
 
 export class SuperGalacticAgeCalculator {
-  constructor(dateOfBirth) {
-
-    this.userDateOfBirth = moment(dateOfBirth);
+  constructor(dateOfBirth, lifeExpectancy) {
+    this.lifeExpectancy = lifeExpectancy;
+    this.dateOfBirth = moment(dateOfBirth);
     //take age in years and convert into seconds
-    this.userAgeInSeconds = (moment().year() - this.userDateOfBirth.year()) * 365 * 24 * 60 * 60;
-
+    this.ageInSeconds = (moment().year() - this.dateOfBirth.year()) * 365 * 24 * 60 * 60;
+    this.mercuryAge = Math.floor(this.ageInSeconds / 7568640);
+    this.venusAge = Math.floor(this.ageInSeconds / 19552320);
+    this.marsAge = Math.floor(this.ageInSeconds / 59287680);
+    this.jupiterAge = Math.floor(this.ageInSeconds / 374016960);
   }
 
   getUserDOB() {
-    return this.userDateOfBirth;
+    return this.dateOfBirth;
   }
 
-  getUserAgeInSeconds() {
-    return this.userAgeInSeconds;
+  getAgeInSeconds() {
+    return this.ageInSeconds;
+  }
+
+  getUserLifeExpectancy() {
+    return this.lifeExpectancy
   }
 
   //take two dates and determine difference in seconds (for ease it is returned as the absolute value of the difference)
@@ -22,28 +29,20 @@ export class SuperGalacticAgeCalculator {
     return  Math.abs(dateOne.format("X") - dateTwo.format("X"));
   }
 
-
-  // 31536000 seconds per earthYear
-
-  // 1 mercuryYear = 0.24 earthYear or 7568640 seconds
-
-  mercuryAge() {
-    return Math.floor(this.userAgeInSeconds / 7568640); // age in earthSeconds/ mercuryYear in seconds rounded down to int value
+  getMercuryAge() {
+    return this.mercuryAge;
   }
 
-  // 1 venusYear = 0.62 earthYear or 19552320 seconds
-  venusAge() {
-    return Math.floor(this.userAgeInSeconds / 19552320);
+  getVenusAge() {
+    return this.venusAge;
   }
 
-  // 1 marsYear = 1.88 earthYear or 59287680 seconds
-  marsAge() {
-    return Math.floor(this.userAgeInSeconds / 59287680);
+  getMarsAge() {
+    return this.marsAge;
   }
 
-  // 1 jupiterYear = 11.86 earthYear or 374016960 seconds
-  jupiterAge() {
-    return Math.floor(this.userAgeInSeconds / 374016960);
+  getJupiterAge() {
+    return this.jupiterAge;
   }
 
   // how many years left to live on each planet
@@ -52,7 +51,7 @@ export class SuperGalacticAgeCalculator {
 
   //BONUS
 
-  // get the date of their next birthday on each planey
+  // get the date of their next birthday on each planet
 
   // return the age of keith richards in dog years on jupiter in 2073
 
