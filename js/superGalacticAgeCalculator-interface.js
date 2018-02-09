e498ee2f216a32152f862ddadfee63e7729e2676
Aaron Ross <ashmortar@gmail.com>
2018-02-09 15:35:36 -0800

    add styling and edit layout of index somewhat

--- js/superGalacticAgeCalculator-interface.js
+++ js/superGalacticAgeCalculator-interface.js
@@ -11,19 +11,19 @@ $(document).ready(function() {
     let dobMomentFormat = moment(dob);
     let doerOfTasks = new SuperGalacticAgeCalculator(dob, lifeExpectancy);
 
-    $('#mercury-stuff').text("<p>Your age in Mercury years is " + doerOfTasks.getMercuryAge() + "</p>");
-    $('#mercury-stuff').text("<p>" + doerOfTasks.getMercuryLE() + "</p>");
+    $('#mercury-stuff').text("Your age in Mercury years is " + doerOfTasks.getMercuryAge() + " and " + doerOfTasks.getMercuryLE());
+    // $('#mercury-stuff').append("<p>" + doerOfTasks.getMercuryLE() + "</p>");
 
-    $('#venus-stuff').text("<p>Your age in Venus years is " + doerOfTasks.getVenusAge() + "</p>");
-    $('#venus-stuff').text("<p>" + doerOfTasks.getVenusLE() + "</p>");
+    $('#venus-stuff').text("Your age in Venus years is " + doerOfTasks.getVenusAge() + " and " + doerOfTasks.getVenusLE());
+    // $('#venus-stuff').append("<p>" + doerOfTasks.getVenusLE() + "</p>");
 
-    $('#mars-stuff').text("<p>Your age in Mars years is " + doerOfTasks.getMarsAge() + "</p>");
-    $('#mars-stuff').text("<p>" + doerOfTasks.getMarsLE() + "</p>");
+    $('#mars-stuff').text("Your age in Mars years is " + doerOfTasks.getMarsAge() + " and " + doerOfTasks.getMarsLE());
+    // $('#mars-stuff').append("<p>" + doerOfTasks.getMarsLE() + "</p>");
 
-    $('#jupiter-stuff').text("<p>Your age in Jupiter years is " + doerOfTasks.getJupiterAge() + "</p>");
-    $('#jupiter-stuff').text("<p>" + doerOfTasks.getJupiterLE() + "</p>");
+    $('#jupiter-stuff').text("Your age in Jupiter years is " + doerOfTasks.getJupiterAge() + " and " + doerOfTasks.getJupiterLE());
+    // $('#jupiter-stuff').append("<p>" + doerOfTasks.getJupiterLE() + "</p>");
 
-    $('#other-stuff').text("<p>You are " + doerOfTasks.compareDates(dobMomentFormat, moment()) + " seconds old (refresh if you dare)");
+    $('#other-stuff').append("<p>You are " + doerOfTasks.compareDates(dobMomentFormat, moment()) + " seconds old (refresh if you dare)");
 
   })
 })
